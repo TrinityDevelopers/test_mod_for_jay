cfa34b0840e649ab247d27b89453cfc2beccdd4c
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2015-07-21 22:36:04 -0500

    Add some basic stuff!

--- jni/main.cpp
+++ jni/main.cpp
@@ -8,7 +8,23 @@
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__))
 
+class Player;
+class ItemInstance;
+class TilePos;
+class Vec3;
+
+void GameMode$useItemOn_real(Player &, ItemInstance*, TilePos const&, signed char, Vec3 const&);
+
+void GameMode$useItemOn_hook(Player& player, ItemInstance* item, TilePos const& tilepos, signed char sc, Vec3 const& vec3) {
+	GameMode$useItemOn_real(player, item, tilepos, sc, vec3);
+}
+
+
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-	LOGI("Hello world!");
+	LOGI("We will make the coolest mod ever!:-)!");
+
+	void* useItemOn = dlsym(RTLD_DEFAULT, “_ZN8GameMode9useItemOnER6PlayerP12ItemInstanceRK7TilePosaRK4Vec3”);	
+	MSHookFunction(useItemOn, (void*) &GameMode$useItemOn_hook, (void**) &GameMode$useItemOn_real);	
+
 	return JNI_VERSION_1_2;
 }
